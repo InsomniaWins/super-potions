f302c7e699686ecbf689ff7e3a95ad5e5eb29c0a
Insomnia_Wins <insomniawins.business@gmail.com>
2024-11-05 15:49:37 -0600

    Initial commit

---
+++ src/client/java/wins/insomnia/super_potions/client/SuperPotionsClient.java
@@ -0,0 +1,10 @@
+package wins.insomnia.super_potions.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SuperPotionsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/wins/insomnia/super_potions/SuperPotions.java
@@ -0,0 +1,10 @@
+package wins.insomnia.super_potions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SuperPotions implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
